fb7b8c3925a2a3f53681783b0de0046ee0dca2bd
Chubasik <tsygankov.maxim@gmail.com>
2017-12-17 16:28:28 +0300

    Unity Project Added

---
+++ JesusVsJudas-unity/Assets/Scripts/Cell.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour {
+
+    public Sprite EmptySprite;
+    public Sprite JesusSprite;
+    public Sprite JudasSprite;
+
+    public CellState State;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void UpdateImage() {
+        Debug.Log("Update image");
+        if (State == CellState.jesus) {
+            transform.GetChild(0).GetComponent<Image>().sprite = JesusSprite;
+        } else if (State == CellState.judas) {
+            transform.GetChild(0).GetComponent<Image>().sprite = JudasSprite;
+        }
+    }
+
+    public void ChangeCellState() {
+        State = GameManager.Instance.currentTurn;
+        UpdateImage();
+        GameManager.Instance.NextTurn();
+    }
+
+
+
+}
---
+++ JesusVsJudas-unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance = null;
+
+    public GameObject GridUI;
+    public CellState currentTurn = CellState.judas;
+
+    private List<List<CellState>> grid = new List<List<CellState>>();
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < 3; i++) {
+            grid.Add(new List<CellState>());
+            for (int j = 0; j < 3; j++) {
+                grid[i].Add(CellState.none);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public CellState CheckWinner() {
+        CellState winner = CellState.none;
+        // row check
+        for (int i = 0; i < 3; i++) {
+            CellState rowWinner = grid[i][0];
+            winner = rowWinner;
+            for (int j = 1; j < 3; j++) {
+                if(grid[i][j] != rowWinner) {
+                    winner = CellState.none;
+                    break;
+                }
+            }
+            if(winner != CellState.none){
+                return winner;
+            }
+        }
+        // column check
+        for (int i = 0; i < 3; i++) {
+            CellState columnWinner = grid[0][i];
+            winner = columnWinner;
+            for (int j = 1; j < 3; j++) {
+                if (grid[j][i] != columnWinner) {
+                    winner = CellState.none;
+                    break;
+                }
+            }
+            if (winner != CellState.none) {
+                return winner;
+            }
+        }
+        // diagonal check
+        CellState diagonalWinner = grid[0][0];
+        winner = diagonalWinner;
+        for (int i = 1; i < 3; i++) {
+            if(grid[i][i] != diagonalWinner) {
+                winner = CellState.none;
+                break;
+            }
+            if (winner != CellState.none) {
+                return winner;
+            }
+        }
+        // reveresed diagonal check
+        CellState reversedDiagonalWinner = grid[0][2];
+        winner = reversedDiagonalWinner;
+        for (int i = 1; i < 3; i++) {
+            if (grid[i][2 - i] != reversedDiagonalWinner) {
+                winner = CellState.none;
+                break;
+            }
+            if (winner != CellState.none) {
+                return winner;
+            }
+        }
+        return CellState.none;
+    }
+
+    public void NextTurn() {
+        if (currentTurn == CellState.jesus) {
+            currentTurn = CellState.judas;
+        } else {
+            currentTurn = CellState.jesus;
+        }
+        UpdateGrid();
+    }
+
+    private void UpdateGrid() {
+        Cell[] cells = GridUI.transform.GetComponentsInChildren<Cell>();
+        for (int i = 0; i < cells.Length; i++) {
+            grid[i % 3][i / 3] = cells[i].State;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Debug.Log(grid[i][j]);
+            }
+        }
+    }
+}
